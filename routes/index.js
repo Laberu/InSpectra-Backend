cb803689662f4a86e09a14308ec16aa81d600f2a
teenteennnii <teennopwijit@gmail.com>
2024-09-13 10:44:10 +0700

    modify index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
+
+// creating a router
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// configuring routes
+router.get("/", function (req, res) {
+  // console.log("request", req)
+  // console.log("response", res)
+  // sending the response
+  res.send("Hello Express!! 👋");
 });
 
 module.exports = router;
