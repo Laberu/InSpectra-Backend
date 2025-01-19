367fe88f90489da36064d6cffdf4c99fc99beef8
teenteennnii <teennopwijit@gmail.com>
2025-01-19 23:25:46 +0700

    modify test

--- test/auth.test.js
+++ test/auth.test.js
@@ -18,14 +18,19 @@ app.use('/auth', authRouter);
 let mongoServer;
 
 beforeAll(async () => {
-  mongoServer = await MongoMemoryServer.create();
-  const uri = mongoServer.getUri();
-  await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  await mongoose.connect('mongodb://localhost:27017/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  await User.deleteMany({});
 });
 
 afterAll(async () => {
   await mongoose.disconnect();
-  await mongoServer.stop();
+  // await mongoServer.stop();
 });
 
 describe('Auth Routes', () => {
